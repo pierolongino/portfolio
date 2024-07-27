969a7b6ee232e42ce55a41853a59d672618ed973
piero <pierolongino.pl@gmail.com>
2024-07-27 22:35:47 +0200

    update form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,30 +7,30 @@ export const Contact = () => {
   return (
     <section className={styles.contact} id='contact'>
         <h2 className={styles.heading}>Contact Me!</h2>
-        <form action="#">
+        <form action="https://formspree.io/f/xgvwaeja" method="POST">
             <div className={styles.inputBox}>
                 <div className={styles.inputField}>
-                    <input type="text" placeholder='Full Name' required />
+                    <input type="text" name='fullName' placeholder='Full Name' required />
                     <span className={styles.focus}></span>
                 </div>
                 <div className={styles.inputField}>
-                    <input type="text" placeholder='Email Address' required />
+                    <input type="text" name='email' placeholder='Email Address' required />
                     <span className={styles.focus}></span>
                 </div>
             </div>
             <div className={styles.inputBox}>
                 <div className={styles.inputField}>
-                    <input type="number" placeholder='Mobile Number' required />
+                    <input type="number" name='mobileNumber' placeholder='Mobile Number' required />
                     <span className={styles.focus}></span>
                 </div>
                 <div className={styles.inputField}>
-                    <input type="text" placeholder='Email Subject' required />
+                    <input type="text" name='subject' placeholder='Email Subject' required />
                     <span className={styles.focus}></span>
                 </div>
             </div>
 
             <div className={styles.textareaField}>
-                <textarea name="" id="" cols={30} rows={10} placeholder='Your Message' required></textarea>
+                <textarea name="message" id="" cols={30} rows={10} placeholder='Your Message' required></textarea>
                 <span className={styles.focus}></span>
             </div>
 
